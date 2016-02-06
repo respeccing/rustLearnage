f38c4e1b40aa15bab71a9a12011c927e0b974ebb
Emanuel Czirai <aoaaxy+github@gmail.com>
2016-02-06 01:07:47 +0100

    hello world from rustbyexample

---
+++ rustbyexample/1./hello.rs
@@ -0,0 +1,18 @@
+//src: https://rustbyexample.com/hello.html
+//XXX: NOTE: to can copy/paste from there ^ you have to about:config dom.event.clipboardevents.enabled;true  which is the default!  Also some keys won't work while inside that frame like Ctrl+T which swaps chars at cursor instead of new firefox tab! I believe this is true for play.rust-lang.org too! https://github.com/rust-lang/rust-playpen/issues/69#issuecomment-155036257
+
+// This is a comment, and will be ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+
