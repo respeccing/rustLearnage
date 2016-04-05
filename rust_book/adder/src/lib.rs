b6930da47bf63ee17f40cba7595c8947001cb1c2
Emanuel Czirai <res.pe.cing+commits@gmail.com>
2016-04-05 16:36:52 +0200

    how to set playground url with cargo doc

--- rust_book/adder/src/lib.rs
+++ rust_book/adder/src/lib.rs
@@ -5,7 +5,26 @@
 //! ```
 //! assert_eq!(4, adder::add_two(2));
 //! ```
+//!
 //! (note: this only works in library crates, not binary crates)
+//!
+//! ```
+//! #[test]
+//! fn it_works3() {
+//!     assert_eq!(4, add_two(2));
+//! }
+//! ```
+
+
+/*#![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
+	   html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
+	   html_root_url = "https://doc.rust-lang.org/nightly/",
+	   html_playground_url = "https://play.rust-lang.org/",
+	   issue_tracker_base_url = "https://github.com/rust-lang/rust/issues/",
+	   test(no_crate_inject, attr(allow(unused_variables), deny(warnings))))]
+       */
+#![doc(html_playground_url = "https://play.rust-lang.org/")]
+//NOTE: ^ rustdoc --markdown-playground-url  WILL NOT WORK! only the above will! see: https://github.com/rust-lang/rust/pull/32746
 
 #![feature(test)]
 extern crate test;
